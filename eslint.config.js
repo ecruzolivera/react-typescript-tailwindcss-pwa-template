f1b93b670b02c514ee6c26c288138770fd33c32b
Ernesto Cruz Olivera <ecruzolivera@gmail.com>
2024-11-02 01:30:10 -0600

    linting tailwindcss

--- eslint.config.js
+++ eslint.config.js
@@ -4,11 +4,12 @@ import reactHooks from "eslint-plugin-react-hooks"
 import reactRefresh from "eslint-plugin-react-refresh"
 import tseslint from "typescript-eslint"
 import eslintPluginPrettier from "eslint-plugin-prettier"
+import tailwindcss from "eslint-plugin-tailwindcss";
 
 export default tseslint.config(
   { ignores: ["dist"] },
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    extends: [js.configs.recommended, ...tseslint.configs.recommended, ...tailwindcss.configs["flat/recommended"]],
     files: ["**/*.{ts,tsx}"],
     languageOptions: {
       ecmaVersion: 2020,
@@ -18,6 +19,7 @@ export default tseslint.config(
       "react-hooks": reactHooks,
       "react-refresh": reactRefresh,
       prettier: eslintPluginPrettier,
+      "tailwindcss": tailwindcss,
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
